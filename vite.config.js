b9e8438cb73ecd8c4abd58f114d0e276b87e13ba
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-02-14 00:36:21 +0100

    Fix MIME type issues in config

--- vite.config.js
+++ vite.config.js
@@ -10,14 +10,20 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        // file extensions and formats
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
       },
     },
-    sourcemap: true,
+    // Disable sourcemap in production
+    sourcemap: false,
   },
   resolve: {
     alias: {
       "@": "/src",
     },
+    extensions: [".js", ".jsx", ".json"],
   },
   server: {
     port: 3000,
